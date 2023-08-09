a8cfe2cc5e38b11e095b2a5bde9ad76a5428064a
Kingsuk Biswas <kingsuk2k@gmail.com>
2023-08-09 10:41:02 +0530

    minor fix

--- routes/data/api.js
+++ routes/data/api.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { createClient } = require('@supabase/supabase-js')
-const fetchTransactions =  require("../../util/transactions");
+const { createClient } = require("@supabase/supabase-js");
+const fetchTransactions = require("../../util/transactions");
 const winston = require("winston");
 const verifyToken = require("../../util/verify");
 
@@ -16,7 +16,9 @@ const logger = createLogger({
 //database config
 const supabaseUrl = "https://vautcfpbwbjpqtwyzmfe.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey, {auth: { persistSession: false },});
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: { persistSession: false },
+});
 
 logger.info("Database connected!!");
 
@@ -37,90 +39,93 @@ const filterarray = (array) => {
 };
 
 // to add the transaction data of respective ethereum wallet address
-router.post("/data",verifyToken, async (req, res) => {
-    try {
-      const data = await fetchTransactions();
-      logger.info("Transaction data fetched..");
-      if (data == null) {
-        logger.warn("No data found!!!");
-        return res.status(400).send("no data found!!!");
-      }
-      const filterdata = filterarray(data);
-      logger.info("Data filtered..");
-      const { error } = await supabase.from("transaction").insert(filterdata);
-      if (error) {
-        logger.error("",error);
-        return res.status(400).json({ error: error.details[0].message });
-      }
-      res.status(201).json({ message: "created data", data: filterdata });
-    } catch (error) {
-      logger.error(error);
-      res.status(500).json({ error: "Internal server error" });
+router.post("/data", verifyToken, async (req, res) => {
+  try {
+    const data = await fetchTransactions();
+    logger.info("Transaction data fetched..");
+    if (data == null) {
+      logger.warn("No data found!!!");
+      return res.status(400).send("no data found!!!");
     }
-  });
-  
-  // to get all the transactions stored in the database
-  router.get("/data",verifyToken, async (req, res) => {
-    try {
-      let { data, error } = await supabase.from("transaction").select();
-      if (error) {
-        logger.error("",error);
-        return res.status(400).json({ error: error.details[0].message });
-      }
-      logger.info("Data fetched from database..")
-      res.status(200).json(data);
-    } catch (error) {
-      logger.error("",error);
-      res.status(500).json({ error: "Internal server error" });
+    const filterdata = filterarray(data);
+    logger.info("Data filtered..");
+    const { error } = await supabase.from("transaction").insert(filterdata);
+    if (error) {
+      logger.error("", error);
+      return res.status(400).json({ error: error.details[0].message });
     }
-  });
-  
-  // to get the most contacted address
-  router.get("/report",verifyToken, async (req, res) => {
-    try {
-      const { data, error } = await supabase
-        .from("transaction")
-        .select("hash,receiver,date")
-        .order("date", { ascending: false })
-        .limit(1000);
-      if (error) {
-        logger.error("",error);
-        return res.status(400).json({ error: error.details[0].message });
-      }
-      logger.info("Data fetched from database..")
-  
-      const toAddressCount = data.reduce((count, transaction) => {
-        const toAddress = transaction.receiver;
-        count[toAddress] = (count[toAddress] || 0) + 1;
-        return count;
-      }, {});
-  
-      let maxToAddress = null;
-      let maxCount = 0;
-  
-      for (const [address, count] of Object.entries(toAddressCount)) {
-        if (count > maxCount) {
-          maxCount = count;
-          maxToAddress = address;
-        }
-      }
-      const reportTransaction = data.find(
-        (transaction) => transaction.receiver === maxToAddress
-      );
-      if (!reportTransaction) {
-        logger.warn("no data found!!!")
-        return res.status(400).json({ message: "no data found!!!!" });
-      }
-      reportTransaction.date = new Date(reportTransaction.date).toLocaleString();
-      res.status(200).json({
-        max_value_txn_hash: reportTransaction.hash,
-        max_txns_with_address: reportTransaction.receiver,
-        date_max_txns: reportTransaction.date,
-      });
-    } catch (error) {
-      logger.error("",error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  });
+    res.status(201).json({ message: "created data", data: filterdata });
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-  module.exports = router;
+// to get all the transactions stored in the database
+router.get("/data", verifyToken, async (req, res) => {
+  try {
+    let { data, error } = await supabase.from("transaction").select();
+    if (error) {
+      logger.error("", error);
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    logger.info("Data fetched from database..");
+    res.status(200).json(data);
+  } catch (error) {
+    logger.error("", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// to get the most contacted address
+router.get("/report", verifyToken, async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("transaction")
+      .select("hash,receiver,date")
+      .order("date", { ascending: false })
+      .limit(1000);
+    if (error) {
+      logger.error("", error);
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    logger.info("Data fetched from database..");
+
+    const toAddressCount = data.reduce((count, transaction) => {
+      const toAddress = transaction.receiver;
+      count[toAddress] = (count[toAddress] || 0) + 1;
+      return count;
+    }, {});
+
+    let maxToAddress = null;
+    let maxCount = 0;
+
+    for (const [address, count] of Object.entries(toAddressCount)) {
+      if (count > maxCount) {
+        maxCount = count;
+        maxToAddress = address;
+      }
+    }
+    const maxAddressTransactions = data.filter(
+      (transaction) => transaction.receiver === maxToAddress
+    );
+
+    maxAddressTransactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+    const reportTransaction = maxAddressTransactions[0];
+    if (!reportTransaction) {
+      logger.warn("no data found!!!");
+      return res.status(400).json({ message: "no data found!!!!" });
+    }
+    reportTransaction.date = new Date(reportTransaction.date).toLocaleString();
+    res.status(200).json({
+      max_value_txn_hash: reportTransaction.hash,
+      max_txns_with_address: reportTransaction.receiver,
+      date_max_txns: reportTransaction.date,
+    });
+  } catch (error) {
+    logger.error("", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
